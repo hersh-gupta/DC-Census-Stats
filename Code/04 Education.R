fdb3c2f21dcbe424ba9d35613ab6c47c4ad1af65
hguptadc <hersh.gupta@dc.gov>
2020-01-29 20:26:45 -0500

    gross rent

---
+++ Code/04 Education.R
@@ -0,0 +1,70 @@
+library(tidyverse)
+library(sf)
+library(sp)
+library(viridis)
+library(hrbrthemes)
+library(tidycensus)
+
+
+#Get variables
+variables <- tibble(name = c("Total", "Less than high school graduate", "High school graduate (includes equivalency)", "Some college or associate's degree", "Bachelor's degree", "Graduate or professional degree"),
+                    id = c("B06009_001","B06009_002","B06009_003","B06009_004","B06009_005","B06009_006"))
+#Get shapefiles
+#dc <- tigris::state_legislative_districts(state = "11", house = "upper", cb = T)
+
+#Get Census data
+acs <- get_acs(geography = "state legislative district (upper chamber)",
+               variables = variables$id,
+               geometry = FALSE, 
+               state = "DC",
+               cb = FALSE, 
+               survey = "acs5"
+)
+
+#Cleanup Census data
+acs_merged <- acs %>%
+  left_join(variables, by = c("variable" = "id")) %>%
+  pivot_wider(id_cols = 1, names_from = 6, values_from = 4) %>% 
+  mutate_at(.vars = 3:7, ~.x/Total) %>%
+  pivot_longer(cols = 3:7) %>% select(GEOID,name,value) %>% mutate(name = factor(name, levels = variables$name[-1]))
+
+#Merge Shapefile and Census data
+dc_merged <- sp::merge(dc, acs_merged, by = "GEOID", duplicateGeoms = TRUE)
+
+#Convert to sf
+dc_sf <- st_as_sf(dc_merged, crs = st_crs("+proj=longlat +datum=WGS84 +no_defs"))
+
+#Plot
+p <- dc_sf %>%
+  ggplot(aes(fill = value)) +
+  geom_sf(color = NA)+
+  coord_sf(crs = 4326, datum = NA, clip = "off") + 
+  facet_grid(~name,labeller = label_wrap_gen(width=20)) +
+  labs(title = "Educational Attainment by Ward", fill = "", 
+       caption = paste0("@gupta_hersh",str_pad("Source: Five Year ACS 2018", width = 250, side = "left")))+
+  scale_fill_viridis_c(#breaks=c(.05,.1,.2,.3,.4,.5,.6,.7),
+                       breaks = waiver(),
+                       #option = "magma",
+                       #trans = "log",
+                       guide = guide_legend(keyheight = unit(3, units = "mm"),
+                                            keywidth=unit(12, units = "mm"),
+                                            label.position = "bottom",
+                                            title.position = "top", nrow=1),
+                       labels = scales::percent_format(accuracy = 1)) +
+  theme_ipsum_pub(base_size = 14, axis = F, ticks = F, grid = F, 
+                  plot_title_margin = margin(0,0,0,0)) +
+  theme(legend.position = "top",
+        legend.box.margin = margin(0,0,0,0),
+        legend.margin = margin(0,0,0,0),
+        plot.caption = element_text(hjust = 0),
+        #panel.spacing = unit(6, "lines")
+        )
+
+p
+#Save
+ggsave(p, filename = here::here("Output","Education.png"), device = "png", dpi = 320, 
+       width = 8, height = 4.5, scale = 1.35)
+
+
+
+
